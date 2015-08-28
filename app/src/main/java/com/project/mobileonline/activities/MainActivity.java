4d178dc5bbb48ffc92c6277f20b592aa7b60467a
Nguyen Dinh Duc <dinhduc0605@gmail.com>
2015-08-29 00:18:29 +0700

    add package adapters

--- app/src/main/java/com/project/mobileonline/activities/MainActivity.java
+++ app/src/main/java/com/project/mobileonline/activities/MainActivity.java
@@ -1,18 +1,44 @@
 package com.project.mobileonline.activities;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 import com.project.mobileonline.R;
+import com.project.mobileonline.models.DrawerItem;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    ListView drawerMenu;
+    ArrayList<DrawerItem> items = new ArrayList<>();
+
+    String[] titles = {"Store", "Categories", "History", "News", "About Us", "Exit"};
+    int[] iconRes = {
+            R.drawable.ic_store_black_48dp,
+            R.drawable.ic_list_black_48dp,
+            R.drawable.ic_history_black_48dp,
+            R.drawable.ic_library_books_black_48dp,
+            R.drawable.ic_info_black_48dp,
+            R.drawable.ic_exit_to_app_black_48dp,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
+    }
+
+    private void initData() {
+        for (int i = 0; i < titles.length; i++) {
+            DrawerItem item = new DrawerItem(iconRes[i], titles[i]);
+            items.add(item);
+        }
     }
 
     @Override
