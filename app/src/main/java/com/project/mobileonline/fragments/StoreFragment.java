c4515565b3cb371506b505f4b1fe1ca3cc096a97
Nguyen Dinh Duc <dinhduc0605@gmail.com>
2015-08-30 21:14:54 +0700

    create navigation drawer

--- app/src/main/java/com/project/mobileonline/fragments/StoreFragment.java
+++ app/src/main/java/com/project/mobileonline/fragments/StoreFragment.java
@@ -1,7 +1,21 @@
 package com.project.mobileonline.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.project.mobileonline.R;
+
 /**
  * Created by Nguyen Dinh Duc on 8/29/2015.
  */
-public class StoreFragment {
+public class StoreFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_store, null);
+    }
 }
